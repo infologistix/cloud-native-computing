8260630f36a1028d7a6debcfea2169f1690c019b
Paul <paul_schmidt@mail.de>
2022-04-09 18:00:23 +0200

    add default main.py

---
+++ docker/rest-api/src/main.py
@@ -0,0 +1,22 @@
+
+from typing import Any
+from venv import create
+from flask import Flask, make_response, request
+from database import BookBase
+from datetime import datetime
+
+bookBase = BookBase()
+app = Flask(__name__)
+
+prefix = lambda r: f"/api/v1{r}"
+
+def createResponse(data: Any, route: str, code:int=200, **kwargs):
+    pass
+
+@app.route(prefix("/"))
+def default():
+    return createResponse(["This is a sample API for books."])
+        
+
+if __name__ == "__main__":
+    app.run(port=8080)
